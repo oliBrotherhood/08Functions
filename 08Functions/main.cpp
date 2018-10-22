b53cd361cf17865e23e5db839873d0e38c565675
Oliver <oliver.brotherhood16@bathspa.ac.uk>
2018-10-22 15:07:17 +0100

    Initial Commit

---
+++ 08Functions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  08Functions
+//
+//  Created by Oliver Brotherhood on 22/10/2018.
+//  Copyright © 2018 Oliver Brotherhood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
